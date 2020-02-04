1eba8a6032439a850d358d48d3a678380d348952
Kyle Kjellberg <kyleawkjell@icloud.com>
2020-02-04 14:34:53 -0700

    working on routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -7,7 +8,8 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 require("./routes/html-routes.js");
 require("./routes/api-routes.js");
