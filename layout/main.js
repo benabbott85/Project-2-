5d85dab3fde6d921e39075232e6e8272a22d7ffb
Katie88 <kathrynhoth@gmail.com>
2020-02-04 11:51:34 -0700

    added handlebars main

---
+++ layout/main.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+		<link rel="stylesheet" href="/assets/css/style.css" type="text/css" />
+        <script src="https://kit.fontawesome.com/f1687098ab.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+		<title>EZTravelPlanner</title>
+		<script src="https://code.jquery.com/jquery.js"></script>
+		<script src="/assets/js/cats.js"></script>
+	</head>
+	<body>
+		{{{ body }}}
+	</body>
+</html>
