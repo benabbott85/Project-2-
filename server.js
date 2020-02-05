dc7c4e5c2c21ef2b40fd19215e62988da3a0485b
Kyle Kjellberg <kyleawkjell@icloud.com>
2020-02-05 11:51:45 -0700

    wiring up routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 8080;
+const htmlRoutes = require("./routes/html-routes.js");
 
 var db = require("./models");
 
@@ -11,8 +12,8 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./routes/html-routes.js");
-require("./routes/api-routes.js");
+app.use(htmlRoutes);
+require("./routes/api-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
