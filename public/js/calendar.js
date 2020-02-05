250653592c78a541e7fb044db83ac1b1e3615340
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-02-05 14:33:03 -0700

    new calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,30 +1,6 @@
-var events = [
-    {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-    {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-    {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
-];
+var winHeight = $(window).height();
+var height = ( winHeight * 16.6666 ) / 100;
+var lineHeight = height + "px";
 
-var settings = {
-    Color: '',
-
-    LinkColor: '',
-
-    NavShow: true,
-
-    NavVertical: false,
-    NavLocation: '',
-
-    DateTimeShow: true,
-
-    DateTimeFormat: 'mmm, yyyy',
-
-    DatetimeLocation: '',
-    EventClick: '',
-    EventTargetWholeDay: false,
-
-    DisabledDays: [],
-    ModelChange: model
-};
-
-var element = document.getElementById('caleandar');
-caleandar(element, events, settings);
+$("li").css("line-height", lineHeight);
+$("li").css("height", height);
