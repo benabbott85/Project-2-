8826845e82aac4e1d6f7de9375c6464eb27f13ee
Katie88 <kathrynhoth@gmail.com>
2020-02-05 14:55:24 -0700

    working on newbudget page

--- db/schema.sql
+++ db/schema.sql
@@ -6,9 +6,9 @@ USE travelfinder_db;
 CREATE TABLE travel (
 id INT AUTO_INCREMENT NOT NULL,
 city_name VARCHAR(250),
-hotel_name VARCHAR(250),
 hotel_cost INT,
-trip_cost INT,
+transportation_cost INT,
 food_cost INT,
+alcohol_cost INT,
 PRIMARY KEY (id)
 );
