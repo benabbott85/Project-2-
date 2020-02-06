cec92b6401512af6e1a7c0dfe22bb9bbca9a3f7c
Katie88 <kathrynhoth@gmail.com>
2020-02-06 09:10:03 -0700

    added calculator linked other pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,19 +6,31 @@ router.get("/", function (req, res) {
 });
 
 router.get("/newbudget", function (req, res) {
-    res.render("newbudget")
+    /*
+    db.Trip.findAll({
+        
+        //do stuff with database
+
+    }).then(function(dbData) {
+       res.render("newbudget", dbData)
+    });
+    */
+    
+    res.render("newbudget", {
+        test: "hello"
+    })
 })
 router.get("/calendar", function (req, res) {
     res.render("calendar")
 })
-router.get("/past", function (req, res) {
+router.get("/pasttrip", function (req, res) {
     res.render("pasttrip")
 })
 router.get("/upcoming", function (req, res) {
     res.render("upcoming")
 })
-router.get("/active", function (req, res) {
-    res.render("active")
+router.get("/activetrip", function (req, res) {
+    res.render("activetrip")
 })
 
 module.exports = router
