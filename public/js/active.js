bd7cad1c81c85ca89ced4aecc8af39ad7ccade19
Kyle Kjellberg <kyleawkjell@icloud.com>
2020-02-07 14:34:49 -0700

    printing local storage

--- public/js/active.js
+++ public/js/active.js
@@ -1,12 +1,18 @@
-var retreivedObject = localStorage.getItem("newTrip");
+const retreivedObject = localStorage.getItem("newTrip");
 console.log("retrievedObject: ", JSON.parse(retreivedObject));
+const parseObj = JSON.parse(retreivedObject);
 
 // const fs = require("fs");
-const pdf = require("html-pdf");
+// const pdf = require("html-pdf");
 // var html = fs.readFileSync('../../../views/partials/newbudget.handlebars', 'utf8');
-var options = { format: 'Letter' };
+// var options = { format: 'Letter' };
 
-pdf.create(html, options).toFile(`testing.pdf`, function (error, result) {
-    if (error) return console.log(error);
-    console.log(`Itinerary created for testing.pdf`)
-});
+// pdf.create(html, options).toFile(`testing.pdf`, function (error, result) {
+//     if (error) return console.log(error);
+//     console.log(`Itinerary created for testing.pdf`)
+// });
+
+$("#resF").append(parseObj.flight);
+$("#resH").append(parseObj.hotel);
+$("#resD").append(parseObj.destination);
+$("#resB").append(parseObj.budget);
