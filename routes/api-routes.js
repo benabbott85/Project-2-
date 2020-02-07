e8323b3c3bbdc0af99f14960be01ca119a1e9c25
Kyle Kjellberg <kyleawkjell@icloud.com>
2020-02-06 19:03:07 -0700

    editing routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,47 +1,12 @@
 // var { Post } = require("../models");
 const router = require("express").Router();
 
-    // router.get("/api/posts/:past", function (req, res) {
-
-    //     Post.findAll({}).then(posts => {
-    //         res.json(post)
-    //     })
-
+router.get("/api/activetrip", function (req, res) {
+    
+    // db.Trips.findAll({}).then(function (dbTrips) {
+    //     res.json(dbTrips);
     // });
 
-    // router.get("/api/posts/:upcoming", function (req, res) {
-
-    //     Post.findAll({}).then(posts => {
-    //         res.json(post)
-    //     })
-
-    // });
-
-    // router.get("/api/posts/:active", function (req, res) {
-
-    //     Post.findAll({}).then(posts => {
-    //         res.json(post)
-    //     })
-
-    // });
-
-
-    // router.post("/api/posts/:past", function (req, res) {
-    //     Post.create(req.body).then(post => {
-
-    //     })
-    // });
-
-    // router.post("/api/posts/:upcoming", function (req, res) {
-    //     Post.create(req.body).then(post => {
-
-    //     })
-    // });
-
-    // router.post("/api/posts/:active", function (req, res) {
-    //     Post.create(req.body).then(post => {
-
-    //     })
-    // });
+});
 
 module.exports = router
